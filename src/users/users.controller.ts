ebe104879e8d4ff4b1f1c3329a62b56f0492a463
Alex Linden <alextl2004@gmail.com>
2024-12-11 15:41:38 -0800

    update conrtoller with userServices

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,40 +6,54 @@ import {
   Body,
   Patch,
   Delete,
-  Query
+  Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users') // /users handles this route
 export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
   // GET /users or /users?role=value returns all users or filtered users
   @Get()
   findAll(@Query('role') role?: 'INTERN' | 'ADMIN' | 'ENGINEER') {
-    if (role) {
-      return [];
-    }
-    return [];
+    return this.usersService.findAll(role);
   }
   // GET /users/:id returns a single user
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.usersService.findOne(+id);
   }
 
   // POST /users creates a new user
   // body: { name: 'John Doe', email: 'john@example.com' }
   @Post()
-  create(@Body() user: {}) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ADMIN' | 'ENGINEER';
+    },
+  ) {
+    return this.usersService.create(user);
   }
   // PATCH /users/:id updates a user
   // body: { name: 'Jane Doe', email: 'jane@example.com' }
   @Patch(':id')
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ADMIN' | 'ENGINEER';
+    },
+  ) {
+    return this.usersService.update(+id, userUpdate);
   }
   // DELETE /users/:id deletes a user
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return { id };
+    return this.usersService.remove(+id);
   }
 }
